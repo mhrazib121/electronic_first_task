e913360593f1327237f7428b0a581f8d128bddcf
razib121 <mhrazib121@gmail.com>
2023-08-19 22:24:37 +0600

    tablet responsive

--- script.js
+++ script.js
@@ -4,10 +4,16 @@ const navItem2Element = document.getElementById("nav-item2");
 const crossElement = document.getElementById("cross");
 const pcDownArrowElement = document.getElementById("PC-down-arrow");
 const submenuContainerElement = document.getElementById("submenu-container");
+const humbergerElement = document.getElementById("humberger");
+const menuItemElement = document.getElementById("nav-item-container");
+const menuCrossElement = document.getElementById("menu-cross");
 
 searchIconElement.addEventListener("click", showSearchBox);
 crossElement.addEventListener("click", closeSearchBox);
 pcDownArrowElement.addEventListener("click", showSubmenu);
+humbergerElement.addEventListener("click", showMenu);
+menuItemElement.addEventListener("click", showMenu);
+menuCrossElement.addEventListener("click", closeMenu);
 
 function showSearchBox() {
   searchBoxElement.style.display = "flex";
@@ -21,4 +27,14 @@ function closeSearchBox() {
 }
 function showSubmenu() {
   submenuContainerElement.style.display = "flex";
+}
+function showMenu() {
+  humbergerElement.style.display = "none";
+  menuItemElement.style.display = "flex";
+  menuCrossElement.style.display = "flex";
+}
+function closeMenu() {
+  menuItemElement.style.display = "none";
+  humbergerElement.style.display = "block";
+  menuCrossElement.style.display = "none";
 }
