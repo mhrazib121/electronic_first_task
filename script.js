d4b71757d779cc27fb722837f57592afffd1657d
razib121 <mhrazib121@gmail.com>
2023-08-19 16:37:51 +0600

    search box showing functionality

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-const searchElement = document.getElementById("search-icon");
+const searchIconElement = document.getElementById("search-icon");
+const searchBoxElement = document.getElementById("search-box");
+const navItem2Element = document.getElementById("nav-item2");
+const crossElement = document.getElementById("cross");
+
+searchIconElement.addEventListener("click", showSearchBox);
+crossElement.addEventListener("click", closeSearchBox);
+
+function showSearchBox() {
+  searchBoxElement.style.display = "flex";
+  navItem2Element.style.display = "none";
+}
+function closeSearchBox() {
+  searchBoxElement.style.display = "none";
+  navItem2Element.style.display = "flex";
+}
