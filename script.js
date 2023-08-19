bbf72a57e0285848d0244a5c916a935020568b2b
razib121 <mhrazib121@gmail.com>
2023-08-19 19:45:00 +0600

    80% of mobile responsive done

--- script.js
+++ script.js
@@ -2,15 +2,23 @@ const searchIconElement = document.getElementById("search-icon");
 const searchBoxElement = document.getElementById("search-box");
 const navItem2Element = document.getElementById("nav-item2");
 const crossElement = document.getElementById("cross");
+const pcDownArrowElement = document.getElementById("PC-down-arrow");
+const submenuContainerElement = document.getElementById("submenu-container");
 
 searchIconElement.addEventListener("click", showSearchBox);
 crossElement.addEventListener("click", closeSearchBox);
+pcDownArrowElement.addEventListener("click", showSubmenu);
 
 function showSearchBox() {
   searchBoxElement.style.display = "flex";
   navItem2Element.style.display = "none";
+  crossElement.style.display = "block";
 }
 function closeSearchBox() {
   searchBoxElement.style.display = "none";
   navItem2Element.style.display = "flex";
+  crossElement.style.display = "none";
+}
+function showSubmenu() {
+  submenuContainerElement.style.display = "flex";
 }
